d6905c5a561e8c3d988ef5c124a8783639facb40
winton <mail@wintoni.us>
2019-07-28 23:31:26 -0400

    Add logger

--- test/storeTest.ts
+++ test/storeTest.ts
@@ -1,7 +1,8 @@
 import { listener } from "@listener-js/listener"
+import { Log } from "@listener-js/log"
 import { store } from "../"
 
-listener({ store })
+listener({ Log, store }, { logAll: true })
 
 beforeEach((): void => { store.state = {} })
 
